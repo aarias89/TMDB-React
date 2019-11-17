c2f3f20e126d45651fe4c52c6894251bcf667edc
aarias89 <andresar11@gmail.com>
2019-11-17 15:11:40 -0500

    test app state on initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { TMDB_API_KEY } from "./KEYS";
 import axios from "axios";
-import YTSearch from "youtube-api-search";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+    favMovies: {}
+  };
+
+  fetchMovieData(key, param) {
+    return axios.get(
+      `https://api.themoviedb.org/3/movie/${param}?api_key=${key}&language=en-US`
+    );
+  }
+
+  componentDidMount() {
+    this.fetchMovieData(TMDB_API_KEY, "popular")
+      .then(res => {
+        this.setState({ favMovies: res.data, isLoaded: true });
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+
+  render() {
+    const { isLoaded } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return <div>{console.log(this.state.favMovies)}</div>;
+    }
+    // return (
+    //   <div>
+    //    <div></div>
+    //   </div>
+    // );
+  }
 }
-
-export default App;
